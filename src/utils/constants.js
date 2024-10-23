35d2dfa78093612e305ec7abe461bc8e1bab79cd
HoangThaiDev <thaindev2000@gmail.com>
2024-10-23 22:00:27 +0700

    add domain vercel

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,7 @@
 /** Những domain được phép truy cập tới tài nguyên của server ở môi trường production*/
 exports.WHITELIST_DOMAINS_PRODUCTION = [
   "https://boutique-ecommerce-gamma.vercel.app",
+  "https://admin-boutique-six.vercel.app",
 ];
 
 /** Những domain được phép truy cập tới tài nguyên của server ở môi trường dev*/
