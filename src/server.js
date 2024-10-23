98170461a1c7a016498e50389efdd84dc3bc52f8
HoangThaiDev <thaindev2000@gmail.com>
2024-10-23 20:45:20 +0700

    Fix server

--- src/server.js
+++ src/server.js
@@ -22,31 +22,33 @@ app.use(cookieParser());
 app.set("trust proxy", 1);
 
 // Create + use server
+const connectServerWithDbs = () => {
+  mongooseConnect(() => {
+    if (env.BUILD_MODE === "dev") {
+      server.listen(env.LOCAL_APP_PORT, (err) => {
+        if (err) {
+          throw new Error("Server starting failled!", err);
+        }
+        console.log(
+          `Hello ${env.AUTHOR}. Server starting with port: ${env.LOCAL_APP_PORT}, host: ${env.LOCAL_APP_HOST}`
+        );
+      });
+    }
 
-mongooseConnect(() => {
-  if (env.BUILD_MODE === "dev") {
-    server.listen(env.LOCAL_APP_PORT, (err) => {
-      if (err) {
-        throw new Error("Server starting failled!", err);
-      }
-      console.log(
-        `Hello ${env.AUTHOR}. Server starting with port: ${env.LOCAL_APP_PORT}, host: ${env.LOCAL_APP_HOST}`
-      );
-    });
-  }
+    if (env.BUILD_MODE === "production") {
+      server.listen(process.env.PORT, (err) => {
+        if (err) {
+          throw new Error("Server starting failled!", err);
+        }
+        console.log(
+          `Hello ${env.AUTHOR}. Server starting with port: ${process.env.PORT}, host: Production`
+        );
+      });
+    }
+  });
+};
 
-  if (env.BUILD_MODE === "production") {
-    server.listen(process.env.PORT, (err) => {
-      if (err) {
-        throw new Error("Server starting failled!", err);
-      }
-      console.log(
-        `Hello ${env.AUTHOR}. Server starting with port: ${process.env.PORT}, host: Production`
-      );
-    });
-  }
-});
-
+mongooseConnect(connectServerWithDbs);
 // Create + use routers
 app.use("/shop", productRouter);
 app.use("/user", userRouter);
